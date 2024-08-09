a5954d4451a2dbdfd0e6c534b8d641c63a8b904b
Parker Nilson <parker.todd.nilson@gmail.com>
2024-08-09 07:57:26 -0700

    use correct docker tag

--- scripts/parkernilson/build-image.sh
+++ scripts/parkernilson/build-image.sh
@@ -2,6 +2,6 @@
 docker buildx build \
   $(yq 'to_entries | map(select(.value|type == "!!str")) |  map(" --build-arg " + .key + "=" + .value) | join("")' 'ansible/vars.yml') \
   --target=production \
-  --tag=parkernilson/supabase-postgres:$(sed -n '/postgres-version/ s/.* "\([^"]*\)"/\1/p' common.vars.pkr.hcl) \
+  --tag=public.ecr.aws/supabase/supabase-postgres:$(sed -n '/postgres-version/ s/.* "\([^"]*\)"/\1/p' common.vars.pkr.hcl) \
   --platform=linux/arm64 \
   .
